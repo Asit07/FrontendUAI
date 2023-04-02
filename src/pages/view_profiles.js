3d9aafc4de4299b1cdc74c3b9f05e8632b60c928
ethilty <spinycucumber@gmail.com>
2023-04-02 09:21:31 -0500

    min gpa filtering!

--- src/pages/view_profiles.js
+++ src/pages/view_profiles.js
@@ -1,4 +1,4 @@
-import { ChakraProvider, Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { ChakraProvider, Box, SimpleGrid, Text, FormControl, FormLabel, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper } from "@chakra-ui/react";
 import Navbar from "@/components/navbar";
 import { useState, useEffect } from "react";
 import connection from "@/connection";
@@ -6,20 +6,34 @@ import connection from "@/connection";
 function Form() {
 
     const [profiles, setProfiles] = useState([]);
+    const [minGPA, setMinGPA] = useState(0.0);
+    
+    const fetch = async () => {
+        // Fetch candidates
+        const { data } = await connection.get("/candidates/", {
+            params: {min_gpa: minGPA},
+        });
+        setProfiles(data);
+    }
 
     useEffect(() => {
-        async function fetchData() {
-            // Fetch candidates
-            const { data } = await connection.get("/candidates/");
-            setProfiles(data);
-        }
-        fetchData();
-    });
+        fetch();
+    }, [minGPA]);
 
     return (
         <ChakraProvider>
             <Navbar />
             <Box maxW="1000px" mx="auto" mt="4" mb="4">
+                <FormControl mb="4">
+                    <FormLabel>Minimum GPA</FormLabel>
+                    <NumberInput value={minGPA} onChange={(value) => setMinGPA(value)}>
+                        <NumberInputField/>
+                        <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                        </NumberInputStepper>
+                    </NumberInput>
+                </FormControl>
                 <SimpleGrid columns={2} spacing={10}>
                     {profiles.map(profile => (
                         <Box borderWidth={2} borderRadius={4} p={4}>
